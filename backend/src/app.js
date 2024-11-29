18f41af39e81d37793b5aed25a5ec1db2730ea30
Marc Garcia <marcgaco@gmail.com>
2024-11-29 12:26:21 +0100

    enpoint create user

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
+/** @format */
 
-const User = require('./models/User');
+require("dotenv").config();
+
+const User = require("./models/User");
+const { v4: uuidv4 } = require("uuid");
 
 const uri = process.env.MONGODB_URI;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(uri) 
-  .then(() => console.log('Conectado a MongoDB')) 
-  .catch(err => console.error('Error al conectar a MongoDB', err));
+mongoose
+  .connect(uri)
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error al conectar a MongoDB", err));
 
 // Importa el paquete de Express
-const express = require('express');
+const express = require("express");
 
 // Crea una instancia de la aplicación Express
 const app = express();
@@ -22,8 +26,45 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Define una ruta básica
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo desde Express!');
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo desde Express!");
+});
+
+app.post("/api/v1/user", async (req, res) => {
+  try {
+    const { name, role, email, password } = req.body;
+
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({
+        error: "Faltan campos obligatorios: name, email, password, role",
+      });
+    }
+
+    const newUser = new User({
+      name,
+      role,
+      email,
+      password,
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json({
+      message: "Usuario creado correctamente",
+      user: {
+        id: savedUser._id,
+        name: savedUser.name,
+        role: savedUser.role,
+        email: savedUser.email,
+        create_date: savedUser.create_date,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      message: "Usuario no creado",
+    });
+    return;
+  }
 });
 
 // Inicia el servidor y escucha en el puerto configurado
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,11 +5,11 @@ const userSchema = new mongoose.Schema({
     _id: {type: String, default: uuidv4 },
     name: { type: String, required: true },
     role: { type: String, enum: ['student','professor'] , required: true },
-    email: { type: String, unique: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    project_picture: { type: String, required: true },
+    user_picture: { type: String },
     assigned_projects: [{type: String, ref:'Project' }],
-    create_date: { type: Date, default: Date.now },
+    create_date: { type: Date, default: Date.now() },
     archive_date: { type: Date},
     modify_date: { type: Date}
 });
