c21dc8a7551910308804479da2fdd74892c1b9eb
Marc Garcia <marcgaco@gmail.com>
2024-12-03 13:18:43 +0100

    added userController

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,4 @@
-/** @format */
-
 require("dotenv").config();
-const User = require("./models/User");
 const uri = process.env.MONGODB_URI;
 const mongoose = require("mongoose");
 const cors = require('cors')
@@ -36,44 +33,6 @@ app.use('/api/v1/skill', require('./routes/skillRoutes'));
 // Define una ruta básica
 app.get("/", (req, res) => {
   res.send("¡Hola, mundo desde Express!");
-});
-
-
-app.post("/api/v1/user", async (req, res) => {
-  try {
-    const { name, role, email, password } = req.body;
-
-    if (!name || !email || !password || !role) {
-      return res.status(400).json({
-        error: "Faltan campos obligatorios: name, email, password, role",
-      });
-    }
-
-    const newUser = new User({
-      name,
-      role,
-      email,
-      password,
-    });
-
-    const savedUser = await newUser.save();
-
-    res.status(201).json({
-      message: "Usuario creado correctamente",
-      user: {
-        id: savedUser._id,
-        name: savedUser.name,
-        role: savedUser.role,
-        email: savedUser.email,
-        create_date: savedUser.create_date,
-      },
-    });
-  } catch (e) {
-    res.status(400).json({
-      message: "Usuario no creado",
-    });
-    return;
-  }
 });
 
 // Inicia el servidor y escucha en el puerto configurado
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,69 @@
+const User = require("../models/User");
+
+const userController = {
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.find().select("-password"); // No enviar passwords
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json({ message: "Error obteniendo usuarios" });
+    }
+  },
+  getUser: async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select("-password"); // No enviar passwords
+        res.status(200).json(user);
+      } catch (err) {
+        res.status(500).json({ message: "Error obteniendo usuario" });
+      }
+  },
+  createUser: async (req, res) => {
+    try {
+      const { name, role, email, password } = req.body;
+
+      if (!name || !email || !password || !role) {
+        return res.status(400).json({
+          error: "Faltan campos obligatorios: name, email, password, role",
+        });
+      }
+
+      const newUser = new User({
+        name,
+        role,
+        email,
+        password,
+      });
+
+      const savedUser = await newUser.save();
+
+      res.status(201).json({
+        message: "Usuario creado correctamente",
+        savedUser,
+      });
+    } catch (e) {
+      res.status(400).json({
+        message: "Usuario no creado",
+      });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body);
+      res.status(200).json(user);
+    } catch (e) {
+      res.status(500).json({
+        message: "Usuario no actualizado",
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "Usuario eliminado" });
+    } catch (e) {
+      res.status(500).json({ message: "No se ha podido eliminar el usuario" });
+    }
+  },
+};
+
+module.exports = userController;
