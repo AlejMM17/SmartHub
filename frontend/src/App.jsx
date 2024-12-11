9085967d87cbdf17ed7a4662b147b94b7fb29e95
Alejandro Martinez <alejmarmor@hotmail.es>
2024-12-11 09:34:05 +0100

    initial frontend components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,82 +1,12 @@
-import { useState } from "react"
-
-const Padre = ({ children }) => {
-
-  return (
-    <div>
-      <p>hola que tal</p>
-      { children }
-    </div>
-  )
-}
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
-
-  // const [tareas, setTareas] = useState([
-  //   {
-  //     id: 0,
-  //     name: "asdf",
-  //     type: "hola",
-  //   },
-  //   {
-  //     id: 1,
-  //     name: "que tal",
-  //     type: "hola",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "como estas",
-  //     type: "hola",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "yeyyy",
-  //     type: "hola",
-  //   }
-  // ])
-
-  // const handleChageInput = (e) => {
-  //   setInput(e.target.value)
-  //   console.log(input)
-  // }
-
-  // const handleDelete = (id) => {
-  //   return setTareas(
-  //     tareas.filter(tarea => tarea.id !== id)
-  //   )
-  // }
-
   return (
     <>
-      {/* <h1>contador: {count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>Añade</button>
-      <input type="text" value={input} onChange={handleChageInput} />
-      <h1>{input}</h1> */}
-
-      {/* {tareas.map((tarea) => {
-        return (
-          <div key={tarea.id} onClick={() => handleDelete(tarea.id)}>
-            <p>{tarea.name}</p>
-            <p>{tarea.type}</p>
-
-          </div>
-        )
-      })} */}
-
-      <Padre>
-        <p>Hijo 1</p>
-      </Padre>
-
-      <Padre>
-        <p>Hijo 2</p>
-      </Padre>
-
-      <Padre>
-        <p>asñlkdfj</p>
-      </Padre>
+      <Link to="*">Ir</Link>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './ui/Sidebar'
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <>
+    <Sidebar/>
+
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
