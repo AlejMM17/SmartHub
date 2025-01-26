4a67d8c707b40151601aecfd93cd8ad926c3e5e9
Alejandro <alejmarmor@hotmail.es>
2025-01-26 20:25:24 +0100

    bobmbo

--- frontend/components/SidebarProfessor.jsx
+++ frontend/components/SidebarProfessor.jsx
@@ -220,7 +220,7 @@ export function SidebarDemo({ children }) {
 export const Logo = () => {
   return (
     <Link
-      href="/"
+      href="/professor/projects"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
       <Brain className="dark:text-white" />
@@ -237,7 +237,7 @@ export const Logo = () => {
 export const LogoIcon = () => {
   return (
     <Link
-      href="#"
+      href="/professor/projects"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
       <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
