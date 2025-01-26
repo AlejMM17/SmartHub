e87c3accb67cac1d3ef8868b6816991b6707b828
Alejandro <alejmarmor@hotmail.es>
2025-01-26 20:44:16 +0100

    fix

--- frontend/components/SidebarProfessor.jsx
+++ frontend/components/SidebarProfessor.jsx
@@ -240,7 +240,8 @@ export const LogoIcon = () => {
       href="/professor/projects"
       className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
     >
-      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    <Brain className="dark:text-white" />
+    
     </Link>
   );
 };
